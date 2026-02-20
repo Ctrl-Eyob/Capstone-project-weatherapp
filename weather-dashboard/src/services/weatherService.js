0df87f774cfdabad822788457c2f2da59860637a
Ctrl-Eyob <eyobaberabekru@gmail.com>
2026-02-20 19:24:08 +0300

    created weatherservice.js

---
+++ weather-dashboard/src/services/weatherService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_WEATHER_KEY;
+
+const api = axios.create({
+  baseURL: "https://api.openweathermap.org/data/2.5",
+});
+
+export const getCurrentWeather = async (city) => {
+  return api.get(
+    `/weather?q=${city}&units=metric&appid=${API_KEY}`
+  );
+};
+
+export const getForecast = async (city) => {
+  return api.get(
+    `/forecast?q=${city}&units=metric&appid=${API_KEY}`
+  );
+};
